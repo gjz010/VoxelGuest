808248dd930de00012f85ec023b385a93bed9608
Joe Kent <thedeadlybutter@gmail.com>
2013-01-29 16:39:03 -0500

    +BlockEventListener

---
+++ src/main/java/com/thevoxelbox/voxelguest/modules/regions/BlockEventListener.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.thevoxelbox.voxelguest.modules.regions;
+
+import org.bukkit.event.Listener;
+
+/**
+ *
+ * @author Joe
+ */
+public class BlockEventListener implements Listener
+{
+
+    private RegionModule regionModule;
+
+    public BlockEventListener(RegionModule regionModule) {
+        this.regionModule = regionModule;
+    }
+    
+    
+    
+}
