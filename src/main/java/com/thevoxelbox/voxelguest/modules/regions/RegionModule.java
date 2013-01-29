fa8906027af5942656eef73c3b5d19abc75bed4c
Joe Kent <thedeadlybutter@gmail.com>
2013-01-29 16:37:14 -0500

    +Region Module

---
+++ src/main/java/com/thevoxelbox/voxelguest/modules/regions/RegionModule.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.thevoxelbox.voxelguest.modules.regions;
+
+import com.thevoxelbox.voxelguest.modules.GuestModule;
+
+/**
+ *
+ * @author Joe
+ */
+public class RegionModule extends GuestModule
+{
+
+    public RegionModule() {
+        setName("Region Module");
+    }
+
+    @Override
+    public void onEnable(){
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable(){
+        super.onDisable();
+    }    
+    
+}
