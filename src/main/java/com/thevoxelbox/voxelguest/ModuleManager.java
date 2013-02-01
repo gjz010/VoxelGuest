a547761e00c444faad8c442064134451044d5649
MikeMatrix <m.braun92@gmail.com>
2013-02-01 04:27:43 +0100

    * Added Configuration Injection System * Changed assert statements to Guava Preconditions.

--- src/main/java/com/thevoxelbox/voxelguest/ModuleManager.java
+++ src/main/java/com/thevoxelbox/voxelguest/ModuleManager.java
@@ -1,265 +1,285 @@
 package com.thevoxelbox.voxelguest;
 
-import com.thevoxelbox.voxelguest.modules.Module;
-import org.bukkit.Bukkit;
-import org.bukkit.event.HandlerList;
-import org.bukkit.event.Listener;
-
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
+
+import com.google.common.base.Preconditions;
+import com.thevoxelbox.voxelguest.modules.Module;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
 
 /**
  * @author Monofraps
  */
 public class ModuleManager      // implements ModuleManager -- TODO: export API stuff
 {
-	// maps module <-> registered event listeners
-	private HashMap<Module, Set<Listener>> registeredModules = new HashMap<>();
+    // maps module <-> registered event listeners
+    private HashMap<Module, Set<Listener>> registeredModules = new HashMap<>();
 
-	/**
-	 * Registers a guest module and enables it immediately if parameter enable is true.
-	 *
-	 * @param module The instance of the module class to register.
-	 * @param enable If set to true, the method will enable the module immediately after registration by calling enableModule
-	 */
-	public final void registerGuestModule(final Module module, final boolean enable)
-	{
-		assert module != null;
+    /**
+     * Registers a guest module and enables it immediately if parameter enable is true.
+     *
+     * @param module
+     *         The instance of the module class to register.
+     * @param enable
+     *         If set to true, the method will enable the module immediately after registration by calling enableModule
+     */
+    public final void registerGuestModule(final Module module, final boolean enable)
+    {
+        Preconditions.checkNotNull(module, "Module must not be null.");
 
-		if (this.registeredModules.containsKey(module))
-		{
-			Bukkit.getLogger().severe(String.format("Module already registered. (Module: %s)", module.toString()));
-			return;
-		}
+        if (this.registeredModules.containsKey(module))
+        {
+            Bukkit.getLogger().severe(String.format("Module already registered. (Module: %s)", module.toString()));
+            return;
+        }
 
-		this.registeredModules.put(module, new HashSet<Listener>());
+        this.registeredModules.put(module, new HashSet<Listener>());
 
-		if (enable)
-		{
-			try
-			{
-				enableModuleByInstance(module);
-			} catch (Exception ex)
-			{
-				Bukkit.getLogger().severe(String.format("Failed to enable module %s", module.toString()));
-				ex.printStackTrace();
-			}
-		}
-	}
+        if (enable)
+        {
+            try
+            {
+                enableModuleByInstance(module);
+            }
+            catch (Exception ex)
+            {
+                Bukkit.getLogger().severe(String.format("Failed to enable module %s", module.toString()));
+                ex.printStackTrace();
+            }
+        }
+    }
 
-	//TODO: Could be made private ...?
-	/**
-	 * Enables a give module.
-	 *
-	 * @param module The instance of the module to enable.
-	 */
-	public final void enableModuleByInstance(final Module module)
-	{
-		assert module != null;
+    //TODO: Could be made private ...?
 
-		final boolean isRegisteredModule = this.registeredModules.containsKey(module);
-		if (!isRegisteredModule)
-		{
-			Bukkit.getLogger().warning("Guest module manager was asked to activate a non-registered module. I will activate the module but cannot keep track of its state and listeners.");
-		}
+    /**
+     * Enables a give module.
+     *
+     * @param module
+     *         The instance of the module to enable.
+     */
+    public final void enableModuleByInstance(final Module module)
+    {
+        Preconditions.checkNotNull(module, "Module must not be null.");
 
-		if (module.isEnabled())
-		{
-			Bukkit.getLogger().warning(String.format("Module already enabled. (Module: %s)", module.toString()));
-			return;
-		}
+        final boolean isRegisteredModule = this.registeredModules.containsKey(module);
+        if (!isRegisteredModule)
+        {
+            Bukkit.getLogger().warning("Guest module manager was asked to activate a non-registered module. I will activate the module but cannot keep track of its state and listeners.");
+        }
 
-		module.onEnable();
+        if (module.isEnabled())
+        {
+            Bukkit.getLogger().warning(String.format("Module already enabled. (Module: %s)", module.toString()));
+            return;
+        }
 
-		// try block prevents loose event listeners from floating around, when the registration fails at some point
-		// it also prevents incomplete modules from staying enabled
-		try
-		{
-			final Set<Listener> moduleListeners = module.getListeners();
+        module.onEnable();
 
-			if (!moduleListeners.isEmpty())
-			{
-				Set<Listener> internalListenerTracker = null;
-				if (isRegisteredModule)
-				{
-					internalListenerTracker = this.registeredModules.get(module);
-				}
+        // try block prevents loose event listeners from floating around, when the registration fails at some point
+        // it also prevents incomplete modules from staying enabled
+        try
+        {
+            final Set<Listener> moduleListeners = module.getListeners();
 
-				int numRegisteredListeners = 0;
-				for (Listener listener : moduleListeners)
-				{
-					Bukkit.getPluginManager().registerEvents(listener, VoxelGuest.getPluginInstance());
-					if (isRegisteredModule)
-					{
-						internalListenerTracker.add(listener);
-					}
-					numRegisteredListeners++;
-				}
+            if (!moduleListeners.isEmpty())
+            {
+                Set<Listener> internalListenerTracker = null;
+                if (isRegisteredModule)
+                {
+                    internalListenerTracker = this.registeredModules.get(module);
+                }
 
-				Bukkit.getLogger().info(String.format("Registered %d event listeners for module %s", numRegisteredListeners, module.toString()));
-			}
-		} catch (Exception ex)
-		{
-			Bukkit.getLogger().severe(String.format("Exception while enabling module: %s", ex.getMessage()));
-			ex.printStackTrace();
-			disableModuleByInstance(module);
-		}
-	}
+                int numRegisteredListeners = 0;
+                for (Listener listener : moduleListeners)
+                {
+                    Bukkit.getPluginManager().registerEvents(listener, VoxelGuest.getPluginInstance());
+                    if (isRegisteredModule)
+                    {
+                        internalListenerTracker.add(listener);
+                    }
+                    numRegisteredListeners++;
+                }
 
-	/**
-	 * Enables all registered modules of type [module]
-	 *
-	 * @param module The type of the modules to enable.
-	 */
-	public final void enableModulesByType(final Class<? extends Module> module)
-	{
-		assert module != null;
+                Bukkit.getLogger().info(String.format("Registered %d event listeners for module %s", numRegisteredListeners, module.toString()));
+            }
+        }
+        catch (Exception ex)
+        {
+            Bukkit.getLogger().severe(String.format("Exception while enabling module: %s", ex.getMessage()));
+            ex.printStackTrace();
+            disableModuleByInstance(module);
+        }
+    }
 
-		for (Module registeredModule : this.registeredModules.keySet())
-		{
-			if (module.isAssignableFrom(registeredModule.getClass()))
-			{
-				try
-				{
-					enableModuleByInstance(registeredModule);
-				} catch (Exception ex)
-				{
-					Bukkit.getLogger().severe(String.format("Exception while enabling module: %s", ex.getMessage()));
-					ex.printStackTrace();
-				}
-			}
-		}
-	}
+    /**
+     * Enables all registered modules of type [module]
+     *
+     * @param module
+     *         The type of the modules to enable.
+     */
+    public final void enableModulesByType(final Class<? extends Module> module)
+    {
+        Preconditions.checkNotNull(module, "Module must not be null.");
 
-	/**
-	 * Disables a give module.
-	 *
-	 * @param module The instance of the module to disable.
-	 */
-	public final void disableModuleByInstance(final Module module)
-	{
-		assert module != null;
+        for (Module registeredModule : this.registeredModules.keySet())
+        {
+            if (module.isAssignableFrom(registeredModule.getClass()))
+            {
+                try
+                {
+                    enableModuleByInstance(registeredModule);
+                }
+                catch (Exception ex)
+                {
+                    Bukkit.getLogger().severe(String.format("Exception while enabling module: %s", ex.getMessage()));
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
 
-		final boolean isRegisteredModule = this.registeredModules.containsKey(module);
-		if (!isRegisteredModule)
-		{
-			Bukkit.getLogger().warning("Guest module manager was asked to disable a non-registered module. I will disable the module but cannot keep track of its state and listeners.");
-		}
+    /**
+     * Disables a give module.
+     *
+     * @param module
+     *         The instance of the module to disable.
+     */
+    public final void disableModuleByInstance(final Module module)
+    {
+        Preconditions.checkNotNull(module, "Module must not be null.");
 
-		if (!module.isEnabled())
-		{
-			Bukkit.getLogger().warning(String.format("Module already disabled. (Module: %s)", module.toString()));
-			return;
-		}
+        final boolean isRegisteredModule = this.registeredModules.containsKey(module);
+        if (!isRegisteredModule)
+        {
+            Bukkit.getLogger().warning("Guest module manager was asked to disable a non-registered module. I will disable the module but cannot keep track of its state and listeners.");
+        }
 
-		try
-		{
-			// unregister the module listeners
-			try
-			{
-				final Set<Listener> moduleListeners = module.getListeners();
-				if (isRegisteredModule)
-				{
-					moduleListeners.addAll(this.registeredModules.get(module));
-					this.registeredModules.get(module).clear();
-				}
+        if (!module.isEnabled())
+        {
+            Bukkit.getLogger().warning(String.format("Module already disabled. (Module: %s)", module.toString()));
+            return;
+        }
 
-				if (!moduleListeners.isEmpty())
-				{
-					for (Listener listener : moduleListeners)
-					{
-						Bukkit.getPluginManager().registerEvents(listener, VoxelGuest.getPluginInstance());
-						HandlerList.unregisterAll(listener);
-					}
-				}
-			} catch (Exception ex)
-			{
-				Bukkit.getLogger().severe("Failed to unregister module listeners.");
-				ex.printStackTrace();
-			} finally
-			{
-				if (isRegisteredModule)
-				{
-					registeredModules.get(module).clear();
-				}
-			}
+        try
+        {
+            // unregister the module listeners
+            try
+            {
+                final Set<Listener> moduleListeners = module.getListeners();
+                if (isRegisteredModule)
+                {
+                    moduleListeners.addAll(this.registeredModules.get(module));
+                    this.registeredModules.get(module).clear();
+                }
 
-			module.onDisable();
-		} catch (Exception ex)
-		{
-			Bukkit.getLogger().severe(String.format("Exception while disabling module: %s", ex.getMessage()));
-			ex.printStackTrace();
-		}
-	}
+                if (!moduleListeners.isEmpty())
+                {
+                    for (Listener listener : moduleListeners)
+                    {
+                        Bukkit.getPluginManager().registerEvents(listener, VoxelGuest.getPluginInstance());
+                        HandlerList.unregisterAll(listener);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Bukkit.getLogger().severe("Failed to unregister module listeners.");
+                ex.printStackTrace();
+            }
+            finally
+            {
+                if (isRegisteredModule)
+                {
+                    registeredModules.get(module).clear();
+                }
+            }
 
-	/**
-	 * Disables all modules of type [module].
-	 *
-	 * @param module The type of the modules to disable.
-	 */
-	public final void disableModulesByType(final Class<? extends Module> module)
-	{
-		assert module != null;
+            module.onDisable();
+        }
+        catch (Exception ex)
+        {
+            Bukkit.getLogger().severe(String.format("Exception while disabling module: %s", ex.getMessage()));
+            ex.printStackTrace();
+        }
+    }
 
-		for (Module registeredModule : this.registeredModules.keySet())
-		{
-			if (module.isAssignableFrom(registeredModule.getClass()))
-			{
-				try
-				{
-					disableModuleByInstance(registeredModule);
-				} catch (Exception ex)
-				{
-					Bukkit.getLogger().severe(String.format("Exception while disabling module: %s", ex.getMessage()));
-					ex.printStackTrace();
-				}
-			}
-		}
-	}
+    /**
+     * Disables all modules of type [module].
+     *
+     * @param module
+     *         The type of the modules to disable.
+     */
+    public final void disableModulesByType(final Class<? extends Module> module)
+    {
+        Preconditions.checkNotNull(module, "Module must not be null.");
 
-	/**
-	 * Restarts or just enables a give module. It calls disableModuleByInstance and enableModuleByInstance internally.
-	 * @param module The instance of the module to restart.
-	 */
-	public void restartModule(final Module module)
-	{
-		assert module != null;
+        for (Module registeredModule : this.registeredModules.keySet())
+        {
+            if (module.isAssignableFrom(registeredModule.getClass()))
+            {
+                try
+                {
+                    disableModuleByInstance(registeredModule);
+                }
+                catch (Exception ex)
+                {
+                    Bukkit.getLogger().severe(String.format("Exception while disabling module: %s", ex.getMessage()));
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
 
-		if (!module.isEnabled())
-		{
-			enableModuleByInstance(module);
-			return;
-		}
+    /**
+     * Restarts or just enables a give module. It calls disableModuleByInstance and enableModuleByInstance internally.
+     *
+     * @param module
+     *         The instance of the module to restart.
+     */
+    public void restartModule(final Module module)
+    {
+        Preconditions.checkNotNull(module, "Module must not be null.");
 
-		try
-		{
-			disableModuleByInstance(module);
-			enableModuleByInstance(module);
-		} catch (Exception ex)
-		{
-			Bukkit.getLogger().severe(String.format("Failed to restart module %s because: %s", module.toString(), ex.getMessage()));
-			ex.printStackTrace();
-		}
-	}
+        if (!module.isEnabled())
+        {
+            enableModuleByInstance(module);
+            return;
+        }
 
-	public final void shutdown()
-	{
-		for (Module module : registeredModules.keySet())
-		{
-			if (module.isEnabled())
-			{
-				try
-				{
-					disableModuleByInstance(module);
-				} catch (Exception ex)
-				{
-					Bukkit.getLogger().severe(String.format("Failed to disable module %s because: %s", module.toString(), ex.getMessage()));
-					ex.printStackTrace();
-				}
-			}
-		}
+        try
+        {
+            disableModuleByInstance(module);
+            enableModuleByInstance(module);
+        }
+        catch (Exception ex)
+        {
+            Bukkit.getLogger().severe(String.format("Failed to restart module %s because: %s", module.toString(), ex.getMessage()));
+            ex.printStackTrace();
+        }
+    }
 
-		registeredModules.clear();
-	}
+    public final void shutdown()
+    {
+        for (Module module : registeredModules.keySet())
+        {
+            if (module.isEnabled())
+            {
+                try
+                {
+                    disableModuleByInstance(module);
+                }
+                catch (Exception ex)
+                {
+                    Bukkit.getLogger().severe(String.format("Failed to disable module %s because: %s", module.toString(), ex.getMessage()));
+                    ex.printStackTrace();
+                }
+            }
+        }
+
+        registeredModules.clear();
+    }
 }
