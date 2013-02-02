e99d5a357811723818a7b6e3be3f60d2a0d5ef26
MikeMatrix <m.braun92@gmail.com>
2013-02-02 02:13:14 +0100

    * Added check for null in Module Manager * Cleaned up ModuleManager code * Added Modules to VoxelGuest registration * Added loggin suppression for hibernate

--- src/main/java/com/thevoxelbox/voxelguest/VoxelGuest.java
+++ src/main/java/com/thevoxelbox/voxelguest/VoxelGuest.java
@@ -1,7 +1,11 @@
 package com.thevoxelbox.voxelguest;
 
 import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import com.thevoxelbox.voxelguest.modules.asshat.AsshatModule;
+import com.thevoxelbox.voxelguest.modules.greylist.GreylistModule;
 import com.thevoxelbox.voxelguest.modules.regions.RegionModule;
 import com.thevoxelbox.voxelguest.persistence.Persistence;
 
@@ -13,57 +17,64 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class VoxelGuest extends JavaPlugin
 {
-	private static VoxelGuest pluginInstance = null;
-	private static ModuleManager moduleManagerInstance = null;
+    private static VoxelGuest pluginInstance = null;
+    private static ModuleManager moduleManagerInstance = null;
 
-	public static VoxelGuest getPluginInstance()
-	{
-		return VoxelGuest.pluginInstance;
-	}
+    public static VoxelGuest getPluginInstance()
+    {
+        return VoxelGuest.pluginInstance;
+    }
 
-	private static void setPluginInstance(VoxelGuest pluginInstance)
-	{
-		if (VoxelGuest.pluginInstance != null)
-		{
-			throw new RuntimeException("Guest Plugin Instance already set.");
-		}
+    private static void setPluginInstance(VoxelGuest pluginInstance)
+    {
+        if (VoxelGuest.pluginInstance != null)
+        {
+            throw new RuntimeException("Guest Plugin Instance already set.");
+        }
 
-		VoxelGuest.pluginInstance = pluginInstance;
-	}
+        VoxelGuest.pluginInstance = pluginInstance;
+    }
 
-	public static ModuleManager getModuleManagerInstance()
-	{
-		return moduleManagerInstance;
-	}
+    public static ModuleManager getModuleManagerInstance()
+    {
+        return moduleManagerInstance;
+    }
 
-	public static void setModuleManagerInstance(final ModuleManager moduleManagerInstance)
-	{
-		if (VoxelGuest.moduleManagerInstance != null)
-		{
-			throw new RuntimeException("Guest Module Manger Instance already set.");
-		}
+    public static void setModuleManagerInstance(final ModuleManager moduleManagerInstance)
+    {
+        if (VoxelGuest.moduleManagerInstance != null)
+        {
+            throw new RuntimeException("Guest Module Manger Instance already set.");
+        }
 
-		VoxelGuest.moduleManagerInstance = moduleManagerInstance;
-	}
-
-	@Override
-	public void onDisable()
-	{
-		VoxelGuest.getModuleManagerInstance().shutdown();
-	}
-
-	@Override
-	public void onEnable()
-	{
-		VoxelGuest.setPluginInstance(this);
-		VoxelGuest.setModuleManagerInstance(new ModuleManager());
-
-        VoxelGuest.getModuleManagerInstance().registerGuestModule(new RegionModule(), false);
+        VoxelGuest.moduleManagerInstance = moduleManagerInstance;
     }
 
     @Override
     public void onLoad()
     {
         Persistence.getInstance().initialize(new File(getDataFolder(), "persistence.db"));
+    }
+
+    @Override
+    public void onDisable()
+    {
+        VoxelGuest.getModuleManagerInstance().shutdown();
+    }
+
+    @Override
+    public void onEnable()
+    {
+        VoxelGuest.setPluginInstance(this);
+        VoxelGuest.setModuleManagerInstance(new ModuleManager());
+
+        VoxelGuest.getModuleManagerInstance().registerGuestModule(new RegionModule(), true);
+        VoxelGuest.getModuleManagerInstance().registerGuestModule(new AsshatModule(), true);
+        VoxelGuest.getModuleManagerInstance().registerGuestModule(new GreylistModule(), true);
+    }
+
+    static
+    {
+        Logger.getLogger("org.hibernate").setLevel(Level.WARNING);
     }
 }
