6ecd445b1bbe37a334855947274c1d3a34cd387f
Monofraps <nic_ristock@live.de>
2013-02-02 02:17:42 +0100

    Merged and cleaned up butters

--- src/main/java/com/thevoxelbox/voxelguest/modules/regions/RegionModule.java
+++ src/main/java/com/thevoxelbox/voxelguest/modules/regions/RegionModule.java
@@ -1,18 +1,19 @@
 package com.thevoxelbox.voxelguest.modules.regions;
 
-import com.thevoxelbox.voxelguest.modules.regions.listener.PlayerEventListener;
-import com.thevoxelbox.voxelguest.modules.regions.listener.BlockEventListener;
 import com.thevoxelbox.voxelguest.modules.GuestModule;
+import com.thevoxelbox.voxelguest.modules.regions.command.RegionCommand;
+import com.thevoxelbox.voxelguest.modules.regions.listener.BlockEventListener;
+import com.thevoxelbox.voxelguest.modules.regions.listener.PlayerEventListener;
 import com.thevoxelbox.voxelguest.persistence.Persistence;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.event.Listener;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.command.CommandExecutor;
 
 /**
  * @author Butters
@@ -21,55 +22,51 @@ public class RegionModule extends GuestModule
 {
 	private List<Region> regions = new ArrayList<>();
 	private BlockEventListener blockEventListener;
-        private PlayerEventListener playerEventListener;
-        private RegionCommand regionCommand;
+	private PlayerEventListener playerEventListener;
+	private RegionCommand regionCommand;
 
 	public RegionModule()
 	{
 		setName("Region Module");
 
 		blockEventListener = new BlockEventListener(this);
-                playerEventListener = new PlayerEventListener(this);
-                regionCommand = new RegionCommand(this);
+		playerEventListener = new PlayerEventListener(this);
+		regionCommand = new RegionCommand(this);
 	}
 
 	@Override
 	public final void onEnable()
-	{		
+	{
 
-	    Persistence.getInstance().registerPersistentClass(Region.class);
+		Persistence.getInstance().registerPersistentClass(Region.class);
 
-	    regions.clear();
-	    List<Object> protoList = Persistence.getInstance().loadAll(Region.class);
-	    for (Object protoRegion : protoList)
-	    {
-		    regions.add((Region) protoRegion);
-	    }
+		regions.clear();
+		List<Object> protoList = Persistence.getInstance().loadAll(Region.class);
+		for (Object protoRegion : protoList)
+		{
+			regions.add((Region) protoRegion);
+		}
 
-	    super.onEnable();
+		super.onEnable();
 	}
 
 	@Override
 	public final void onDisable()
 	{
-	    regions.clear();
-	    List<Object> protoList = new ArrayList<>();
-	    for (Region region : regions)
-	    {
-		    protoList.add(region);
-	    }
+		regions.clear();
+		List<Object> protoList = new ArrayList<>();
+		for (Region region : regions)
+		{
+			protoList.add(region);
+		}
 
 		super.onDisable();
 	}
 
 	@Override
-	public final HashSet<Listener> getListeners()
+	public String getConfigFileName()
 	{
-		final HashSet<Listener> listeners = new HashSet<>();
-		listeners.add(blockEventListener);
-                listeners.add(blockEventListener);
-
-		return listeners;
+		return "region";
 	}
 
 	@Override
@@ -79,29 +76,33 @@ public class RegionModule extends GuestModule
 	}
 
 	@Override
-	public String getConfigFileName()
+	public final HashSet<Listener> getListeners()
 	{
-		return "region";
+		final HashSet<Listener> listeners = new HashSet<>();
+		listeners.add(blockEventListener);
+		listeners.add(playerEventListener);
+
+		return listeners;
 	}
-        
-        @Override
-        public HashMap<String, CommandExecutor> getCommandMappings()
-        {
-                HashMap<String, CommandExecutor> commandMappings = new HashMap<>();
-                commandMappings.put("vgregion", regionCommand);
-                return commandMappings;
-        }
-        
-        public boolean addRegion(Region region)
-        {
-            if(region != null)
-            {
-                regions.add(region);
-                Bukkit.getLogger().info("Created region: " + region.getRegionName());
-                return true;
-            }
-            return false;
-        }
+
+	@Override
+	public HashMap<String, CommandExecutor> getCommandMappings()
+	{
+		HashMap<String, CommandExecutor> commandMappings = new HashMap<>();
+		commandMappings.put("vgregion", regionCommand);
+		return commandMappings;
+	}
+
+	public boolean addRegion(Region region)
+	{
+		if (region != null)
+		{
+			regions.add(region);
+			Bukkit.getLogger().info("Created region: " + region.getRegionName());
+			return true;
+		}
+		return false;
+	}
 
 	public final Region getRegionAtLocation(final Location regionLocation)
 	{
