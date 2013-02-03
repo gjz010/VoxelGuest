4b383aa10bd11285c6c3f6656a48f018811580a6
Monofraps <nic_ristock@live.de>
2013-02-03 08:13:25 +0100

    Added recreation of session factory prevention

--- src/main/java/com/thevoxelbox/voxelguest/modules/greylist/GreylistModule.java
+++ src/main/java/com/thevoxelbox/voxelguest/modules/greylist/GreylistModule.java
@@ -1,9 +1,5 @@
 package com.thevoxelbox.voxelguest.modules.greylist;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-
 import com.google.common.base.Preconditions;
 import com.thevoxelbox.voxelguest.configuration.annotations.ConfigurationGetter;
 import com.thevoxelbox.voxelguest.configuration.annotations.ConfigurationSetter;
@@ -13,131 +9,143 @@ import com.thevoxelbox.voxelguest.modules.greylist.command.UngreylistCommandExec
 import com.thevoxelbox.voxelguest.modules.greylist.listener.GreylistListener;
 import com.thevoxelbox.voxelguest.modules.greylist.model.Greylistee;
 import com.thevoxelbox.voxelguest.persistence.Persistence;
-
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.event.Listener;
 import org.hibernate.criterion.Restrictions;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 
 /**
  * @author MikeMatrix
  */
 public class GreylistModule extends GuestModule
 {
-    private GreylistListener greylistListener;
-    private GreylistCommandExecutor greylistCommandExecutor;
-    private UngreylistCommandExecutor ungreylistCommandExecutor;
-    private boolean explorationMode = false;
-    private String notGreylistedKickMessage = "You are not greylisted.";
+	private GreylistListener greylistListener;
+	private GreylistCommandExecutor greylistCommandExecutor;
+	private UngreylistCommandExecutor ungreylistCommandExecutor;
+	private boolean explorationMode = false;
+	private String notGreylistedKickMessage = "You are not greylisted.";
 
-    public GreylistModule()
-    {
-        setName("Greylist Module");
-        greylistListener = new GreylistListener(this);
-        greylistCommandExecutor = new GreylistCommandExecutor(this);
-        ungreylistCommandExecutor = new UngreylistCommandExecutor(this);
+	public GreylistModule()
+	{
+		setName("Greylist Module");
+		greylistListener = new GreylistListener(this);
+		greylistCommandExecutor = new GreylistCommandExecutor(this);
+		ungreylistCommandExecutor = new UngreylistCommandExecutor(this);
 
-	    Persistence.getInstance().registerPersistentClass(Greylistee.class);
-    }
+		Persistence.getInstance().registerPersistentClass(Greylistee.class);
+	}
 
-    @Override
-    public Object getConfiguration()
-    {
-        return this;
-    }
+	@Override
+	public void onEnable()
+	{
 
-    @Override
-    public void onEnable()
-    {
+		super.onEnable();
+	}
 
-        super.onEnable();
-    }
+	@Override
+	public Object getConfiguration()
+	{
+		return this;
+	}
 
-    @Override
-    public HashSet<Listener> getListeners()
-    {
-        final HashSet<Listener> listeners = new HashSet<>();
-        listeners.add(greylistListener);
-        return listeners;
-    }
+	@Override
+	public HashSet<Listener> getListeners()
+	{
+		final HashSet<Listener> listeners = new HashSet<>();
+		listeners.add(greylistListener);
+		return listeners;
+	}
 
-    @Override
-    public HashMap<String, CommandExecutor> getCommandMappings()
-    {
-        HashMap<String, CommandExecutor> commandMapping = new HashMap<>();
-        commandMapping.put("greylist", greylistCommandExecutor);
-        commandMapping.put("ungreylist", ungreylistCommandExecutor);
-        return commandMapping;
-    }
+	@Override
+	public HashMap<String, CommandExecutor> getCommandMappings()
+	{
+		HashMap<String, CommandExecutor> commandMapping = new HashMap<>();
+		commandMapping.put("greylist", greylistCommandExecutor);
+		commandMapping.put("ungreylist", ungreylistCommandExecutor);
+		return commandMapping;
+	}
 
-    @ConfigurationGetter("exploration-mode")
-    public boolean isExplorationMode()
-    {
-        return explorationMode;
-    }
+	@ConfigurationGetter("exploration-mode")
+	public boolean isExplorationMode()
+	{
+		return explorationMode;
+	}
 
-    @ConfigurationSetter("exploration-mode")
-    public void setExplorationMode(final boolean explorationMode)
-    {
-        this.explorationMode = explorationMode;
-    }
+	@ConfigurationSetter("exploration-mode")
+	public void setExplorationMode(final boolean explorationMode)
+	{
+		this.explorationMode = explorationMode;
+	}
 
-    @ConfigurationGetter("not-greylisted-kick-message")
-    public String getNotGreylistedKickMessage()
-    {
-        return notGreylistedKickMessage;
-    }
+	@ConfigurationGetter("not-greylisted-kick-message")
+	public String getNotGreylistedKickMessage()
+	{
+		return notGreylistedKickMessage;
+	}
 
-    @ConfigurationSetter("not-greylisted-kick-message")
-    public void setNotGreylistedKickMessage(final String notGreylistedKickMessage)
-    {
-        this.notGreylistedKickMessage = notGreylistedKickMessage;
-    }
+	@ConfigurationSetter("not-greylisted-kick-message")
+	public void setNotGreylistedKickMessage(final String notGreylistedKickMessage)
+	{
+		this.notGreylistedKickMessage = notGreylistedKickMessage;
+	}
 
-    public final boolean isOnPersistentGreylist(String name)
-    {
-        final List<Object> greylistees = Persistence.getInstance().loadAll(Greylistee.class, Restrictions.like("name", name));
+	public final boolean isOnPersistentGreylist(String name)
+	{
+		final List<Object> greylistees;
 
-        for (Object greylisteeObject : greylistees)
-        {
-            if (greylisteeObject instanceof Greylistee)
-            {
-                if (((Greylistee) greylisteeObject).getName().equalsIgnoreCase(name))
-                {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+		try
+		{
+			greylistees = Persistence.getInstance().loadAll(Greylistee.class, Restrictions.like("name", name));
+		} catch (Exception ex)
+		{
+			ex.printStackTrace();
+			return false;
+		}
 
-    public void greylist(final String name)
-    {
-        final List<Object> greylistees = Persistence.getInstance().loadAll(Greylistee.class, Restrictions.like("name", name));
-        for (Object greylisteeObject : greylistees)
-        {
-            Preconditions.checkState(greylisteeObject instanceof Greylistee);
+		for (Object greylisteeObject : greylistees)
+		{
+			if (greylisteeObject instanceof Greylistee)
+			{
+				if (((Greylistee) greylisteeObject).getName().equalsIgnoreCase(name))
+				{
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 
-            Greylistee greylistee = (Greylistee) greylisteeObject;
-            if (greylistee.getName().equalsIgnoreCase(name))
-            {
-                return;
-            }
-        }
-        Persistence.getInstance().save(new Greylistee(name));
-    }
+	public void greylist(final String name)
+	{
+		final List<Object> greylistees = Persistence.getInstance().loadAll(Greylistee.class, Restrictions.like("name", name));
+		for (Object greylisteeObject : greylistees)
+		{
+			Preconditions.checkState(greylisteeObject instanceof Greylistee);
 
-    public void ungreylist(final String name)
-    {
-        final List<Object> greylistees = Persistence.getInstance().loadAll(Greylistee.class, Restrictions.like("name", name));
-        for (Object greylisteeObject : greylistees)
-        {
-            Preconditions.checkState(greylisteeObject instanceof Greylistee);
+			Greylistee greylistee = (Greylistee) greylisteeObject;
+			if (greylistee.getName().equalsIgnoreCase(name))
+			{
+				return;
+			}
+		}
+		Persistence.getInstance().save(new Greylistee(name));
+	}
 
-            Greylistee greylistee = (Greylistee) greylisteeObject;
-            if (greylistee.getName().equalsIgnoreCase(name))
-            {
-                Persistence.getInstance().delete(greylistee);
-            }
-        }
-    }
+	public void ungreylist(final String name)
+	{
+		final List<Object> greylistees = Persistence.getInstance().loadAll(Greylistee.class, Restrictions.like("name", name));
+		for (Object greylisteeObject : greylistees)
+		{
+			Preconditions.checkState(greylisteeObject instanceof Greylistee);
+
+			Greylistee greylistee = (Greylistee) greylisteeObject;
+			if (greylistee.getName().equalsIgnoreCase(name))
+			{
+				Persistence.getInstance().delete(greylistee);
+			}
+		}
+	}
 }
--- src/main/java/com/thevoxelbox/voxelguest/persistence/Persistence.java
+++ src/main/java/com/thevoxelbox/voxelguest/persistence/Persistence.java
@@ -1,9 +1,5 @@
 package com.thevoxelbox.voxelguest.persistence;
 
-import java.io.File;
-import java.io.Serializable;
-import java.util.List;
-
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -12,116 +8,123 @@ import org.hibernate.criterion.Criterion;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
 
+import java.io.File;
+import java.io.Serializable;
+import java.util.List;
+
 /**
  * @author MikeMatrix
  */
 public class Persistence
 {
-    private static Persistence instance = new Persistence();
-    private Configuration configuration = new Configuration();
-    private SessionFactory sessionFactory;
+	private static Persistence instance = new Persistence();
+	private Configuration configuration = new Configuration();
+	private SessionFactory sessionFactory = null;
 
-    private Persistence()
-    {
-    }
+	private Persistence()
+	{
+	}
 
-    public static Persistence getInstance()
-    {
-        return instance;
-    }
+	public static Persistence getInstance()
+	{
+		return instance;
+	}
 
-    public void initialize(final File file)
-    {
-        file.getParentFile().mkdirs();
-        configuration
-                .setProperty("hibernate.dialect", "org.hibernate.dialect.SQLiteDialect")
-                .setProperty("hibernate.connection.driver_class", "org.sqlite.JDBC")
-                .setProperty("hibernate.connection.url", "jdbc:sqlite:" + file.getPath())
-                .setProperty("hibernate.hbm2ddl.auto", "update");
-    }
+	public void initialize(final File file)
+	{
+		file.getParentFile().mkdirs();
+		configuration
+				.setProperty("hibernate.dialect", "org.hibernate.dialect.SQLiteDialect")
+				.setProperty("hibernate.connection.driver_class", "org.sqlite.JDBC")
+				.setProperty("hibernate.connection.url", "jdbc:sqlite:" + file.getPath())
+				.setProperty("hibernate.hbm2ddl.auto", "update");
+	}
 
-    public void registerPersistentClass(Class<?> persistentClass)
-    {
-        configuration.addAnnotatedClass(persistentClass);
-    }
+	public void registerPersistentClass(Class<?> persistentClass)
+	{
+		configuration.addAnnotatedClass(persistentClass);
+	}
 
-    public void rebuildSessionFactory()
-    {
-        ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
-        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-    }
+	public void rebuildSessionFactory()
+	{
+		if (sessionFactory == null)
+		{
+			ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+			sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+		}
+	}
 
-    public void save(Object object)
-    {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
+	public void save(Object object)
+	{
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
 
-        session.saveOrUpdate(object);
+		session.saveOrUpdate(object);
 
-        session.getTransaction().commit();
-        session.close();
-    }
+		session.getTransaction().commit();
+		session.close();
+	}
 
-    public void saveAll(List<Object> objects)
-    {
-	    Session session = sessionFactory.openSession();
-        session.beginTransaction();
+	public void saveAll(List<Object> objects)
+	{
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
 
-        for (Object object : objects)
-        {
-            session.saveOrUpdate(object);
-        }
+		for (Object object : objects)
+		{
+			session.saveOrUpdate(object);
+		}
 
-	    session.getTransaction().commit();
-        session.close();
-    }
+		session.getTransaction().commit();
+		session.close();
+	}
 
-    public Object load(Class<?> clazz, Serializable id)
-    {
-	    Session session = sessionFactory.openSession();
+	public Object load(Class<?> clazz, Serializable id)
+	{
+		Session session = sessionFactory.openSession();
 
-        Object result = session.load(clazz, id);
+		Object result = session.load(clazz, id);
 
-        session.close();
+		session.close();
 
-        return result;
-    }
+		return result;
+	}
 
-    public List<Object> loadAll(Class<?> clazz)
-    {
-	    Session session = sessionFactory.openSession();
+	public List<Object> loadAll(Class<?> clazz)
+	{
+		Session session = sessionFactory.openSession();
 
-        final List result = session.createCriteria(clazz).list();
+		final List result = session.createCriteria(clazz).list();
 
-        session.close();
+		session.close();
 
-        return result;
-    }
+		return result;
+	}
 
-    public List<Object> loadAll(Class<?> clazz, Criterion... criterion)
-    {
-	    Session session = sessionFactory.openSession();
+	public List<Object> loadAll(Class<?> clazz, Criterion... criterion)
+	{
+		Session session = sessionFactory.openSession();
 
-        final Criteria criteria = session.createCriteria(clazz);
-        for (Criterion currentCriterion : criterion)
-        {
-            criteria.add(currentCriterion);
-        }
-        final List result = criteria.list();
+		final Criteria criteria = session.createCriteria(clazz);
+		for (Criterion currentCriterion : criterion)
+		{
+			criteria.add(currentCriterion);
+		}
+		final List result = criteria.list();
 
-        session.close();
+		session.close();
 
-        return result;
-    }
+		return result;
+	}
 
-    public void delete(final Object greylistee)
-    {
-	    Session session = sessionFactory.openSession();
-        session.beginTransaction();
+	public void delete(final Object greylistee)
+	{
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
 
-        session.delete(greylistee);
+		session.delete(greylistee);
 
-        session.getTransaction().commit();
-        session.close();
-    }
+		session.getTransaction().commit();
+		session.close();
+	}
 }
