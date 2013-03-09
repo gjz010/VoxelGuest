62c458b32b738081b5979bfd3f7159aaf2b43874
Monofraps <nic_ristock@live.de>
2013-03-09 23:38:19 +0100

    Changed configuration file default naming

--- src/main/java/com/thevoxelbox/voxelguest/modules/GuestModule.java
+++ src/main/java/com/thevoxelbox/voxelguest/modules/GuestModule.java
@@ -57,7 +57,7 @@ public abstract class GuestModule implements Module
     @Override
     public String getConfigFileName()
     {
-        return getName().replace(" ", "").toLowerCase();
+        return getName().replace(" ", "");
     }
 
     @Override
